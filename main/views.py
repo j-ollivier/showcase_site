1d933a50bf6cf3e355ebdfbe606d3c7515233b67
Sighill <voxclam.indes@gmail.com>
2018-03-19 06:14:22 +0000

    1st working dev version

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import *
+from django.utils import timezone
 
-# Create your views here.
+#####################################################################
+def LandingPage(request):
+    '''
+        Provides the main landing page information.
+    '''
+    context = {
+        'page_title': 'Julien Ollivier',
+        # 'main_page_news': main_page_news,
+    }
+    template = loader.get_template('main/landingpage.html')
+    return HttpResponse(template.render(context, request))
